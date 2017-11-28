8cd8966a7cbd80f1d2f4e52801f531de01980eab
Mees Gelein <meesgelein@gmail.com>
2017-11-28 13:55:52 +0100

    Removed deprecated highlighting stuff

--- dist/comparativus.min.comp.js
+++ dist/comparativus.min.comp.js
@@ -391,19 +391,6 @@ var comparativus = {
         },
 
         /**
-         * Shakes the fileInput with the provided Name
-         */
-        shakeFileInput: function(name){
-            var fInput = $('#panel' + name.toUpperCase());
-            //Start animating it
-            fInput.addClass('animated jello');
-            //Once the animation ends, remove the animated class
-            fInput.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-              fInput.removeClass('animated jello')
-            });
-        },
-
-        /**
          * Sets the similarity spane value to the provided value
          */
         setSimilarityScore: function(val){
@@ -432,77 +419,6 @@ var comparativus = {
             el.value = Math.round(Number(el.value));
             if(el.value == 0) el.value = 10;
         },
-
-        /**
-         * Highlights a match everywhere (in the visualization, in the text and the resultTable)
-         */
-        highlightEverywhere(linkID, enabled){
-            //linkID is made of indexA + indexB
-            highlightResult(linkID, enabled);
-            highLightMatch($('#S' + linkID), enabled);
-            comparativus.visualization.highlightPair(linkID, enabled);
-        },
-
-        /**
-         * Enables or disables highlighting of the specified row in the resultTable
-         */
-        highlightResult: function(linkID, enabled){
-            var row = $('#row' + linkID);
-            if(enabled){
-                row.addClass('success');
-                row.get(0).scrollIntoView(false);
-            }else{
-                row.removeClass('success');
-            }
-        },
-
-        /**
-         * Highligths both the A and B occurences of the match
-         */
-        highLightMatch: function(element, enabled){
-            var id = '#' + $(element).attr('id');
-            //Highlight both the a and b version
-            comparativus.ui.highLightSingleMatch($(id.replace(/.$/, 'a')).get(0), enabled);
-            comparativus.ui.highLightSingleMatch($(id.replace(/.$/, 'b')).get(0), enabled);
-        },
-
-        /**
-         * Highlight one single match. The provided element is the element the mouse hovered over
-         */
-        highLightSingleMatch: function(element, enabled){
-            var elID = $(element).attr('id');
-            var otherName = (elID.startsWith("S") ? "E" : "S") + elID.substring(1);
-            var startMatch = (elID.startsWith("S") ? ('#' + elID) : ('#' + otherName));
-            var endMatch = (elID.startsWith("S") ? ('#' + otherName) : ('#' + elID));
-
-            //Only contents() also picks up on text nodes. 
-            var id; var adding = false;
-            var selection = $();
-            $(startMatch).parent().contents().each(function(){
-                if('#' + $(this).attr('id') == startMatch){//We found the beginning
-                    adding = true;
-                }else if('#' + $(this).attr('id') == endMatch){
-                    adding = false;
-                    selection = selection.add($(this));
-                    return false;
-                }
-                if(adding) selection = selection.add($(this));
-            });
-            
-            if(enabled){
-                selection.wrap('<span class="selectedSpan">');
-                //Scroll each of the selected spans into view
-                $('.selectedSpan').each(function(){$(this).get(0).scrollIntoView(false)});
-                $(endMatch).addClass('selected');
-                $(startMatch).addClass('selected');
-            }else{
-                $(endMatch).removeClass('selected');
-                $(startMatch).removeClass('selected');
-                //remove any selected span that was found
-                $('.selectedSpan').contents().unwrap();
-            }
-        },
-
         /**
          * Highlights all matches in the text from the linkID
          */
@@ -518,27 +434,27 @@ var comparativus = {
          * the result table
          */
         showResultTable: function(matches){            
-        //now show all matches in the browser
-        var parts = [];
-        parts.push("<thead><tr><th>IndexA</th><th>IndexB</th><th>Length</th><th>TextA</th><th>TextB</th></tr></thead><tbody>");
-        var tsvParts = [];
-        var cMatch; var max = matches.length;
-        $('#matchesAmt').html(max);
-        for(var i = 0; i < max; i++){
-            cMatch = matches[i];
-            var linkID = 'A' + cMatch.indexA + 'B' + cMatch.indexB;
-            parts.push("<tr id='row" + linkID +"'><td><a class='matchLink' onmouseup='comparativus.ui.highlightMatchFromLinkID(\"" + linkID + "\", true);'" + i + 'a' + "'>" + cMatch.indexA +
-            "</a></td><td><a class='matchLink' onmouseup='comparativus.ui.highlightMatchFromLinkID(\"" + linkID + "\", true);'" + i + 'a' + "'>" + cMatch.indexB +
-            "</td><td>" + cMatch.l + "</td><td>" + cMatch.textA + "</td><td>"
-            + cMatch.textB + "</td></tr>");
-            tsvParts.push(cMatch.indexA + '\t' + cMatch.indexB + '\t' + cMatch.l + '\t' + cMatch.r + '\t' + cMatch.textA + '\t' + cMatch.textB);
-        }
+            //now show all matches in the browser
+            var parts = [];
+            parts.push("<thead><tr><th>IndexA</th><th>IndexB</th><th>Length</th><th>TextA</th><th>TextB</th></tr></thead><tbody>");
+            var tsvParts = [];
+            var cMatch; var max = matches.length;
+            $('#matchesAmt').html(max);
+            for(var i = 0; i < max; i++){
+                cMatch = matches[i];
+                var linkID = 'A' + cMatch.indexA + 'B' + cMatch.indexB;
+                parts.push("<tr id='row" + linkID +"'><td><a class='matchLink' onmouseup='comparativus.ui.highlightMatchFromLinkID(\"" + linkID + "\", true);'" + i + 'a' + "'>" + cMatch.indexA +
+                "</a></td><td><a class='matchLink' onmouseup='comparativus.ui.highlightMatchFromLinkID(\"" + linkID + "\", true);'" + i + 'a' + "'>" + cMatch.indexB +
+                "</td><td>" + cMatch.l + "</td><td>" + cMatch.textA + "</td><td>"
+                + cMatch.textB + "</td></tr>");
+                tsvParts.push(cMatch.indexA + '\t' + cMatch.indexB + '\t' + cMatch.l + '\t' + cMatch.r + '\t' + cMatch.textA + '\t' + cMatch.textB);
+            }
 
-        $("#resultTable").html(parts.join() + "</tbody>");
+            $("#resultTable").html(parts.join() + "</tbody>");
 
-        //create the downloadButtons
-        $('#downloadTSVButton').click(function(){createTSVFile(tsvParts);});
-        $('#downloadJSONButton').click(function(){comparativus.file.createJSON(matches);});
+            //create the downloadButtons
+            $('#downloadTSVButton').click(function(){createTSVFile(tsvParts);});
+            $('#downloadJSONButton').click(function(){comparativus.file.createJSON(matches);});
         }
     }
 })(comparativus);;/**
